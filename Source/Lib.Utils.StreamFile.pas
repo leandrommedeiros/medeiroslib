651893a1d19c1d0956da64efaa8399a0001c3d73
Leandro Medeiros <leandro.medeiros.br@gmail.com>
2014-11-13 14:57:26 -0200

    Lib.Utils.StreamFile: Novo atributo "Refresh" permite recarregar o arquivo definido em "FileName"; Ao carregar arquivo é mostrada uma mensagem com o tamanho em MB do stream.

--- Source/Lib.Utils.StreamFile.pas
+++ Source/Lib.Utils.StreamFile.pas
@@ -8,48 +8,57 @@ unit Lib.Utils.StreamFile;
 
 interface
 
+{ Bibliotecas para Interface }
 uses
   SysUtils, Classes;
 
-//==| Classe - Arquivo Embutido |===============================================
+{ Classes }
 type
   TStreamFile = class(TComponent)
   private
     //Campos
-    FFileName: String;
+    FFileName : String;
+    FRefresh  : Boolean;
 
     //Variáveis
     DataStream: TMemoryStream;
   protected
     //Métodos Protegidos
-    procedure   SetFileName(AFileName: String);
-    procedure   DefineProperties(Filer: TFiler); override;
-    procedure   LoadData(Reader: TStream);
-    procedure   StoreData(Writer: TStream);
+    procedure SetFileName(AFileName: String);
+    procedure DefineProperties(Filer: TFiler); override;
+    procedure LoadData(Reader: TStream);
+    procedure StoreData(Writer: TStream);
+    procedure RefreshStream(const AValue: Boolean);
   public
     //Métodos públicos
     Constructor Create (Owner: TComponent); override;
     Destructor  Destroy; override;
 
     //Propriedades públicas
-    property    Data: TMemoryStream read DataStream;
+    property Data     : TMemoryStream read DataStream;
   published
     //Propriedades publicadas
-    property    FileName: String read FFileName write SetFileName;
+    property FileName : String  read FFileName write SetFileName;
+    property Refresh  : Boolean read FRefresh  write RefreshStream;
   end;
 
 implementation
 
+{ Bibliotecas para Interface }
+uses
+  Lib.Files, Dialogs;
+
 {*******************************************************************************
 
-                          IMPLEMENTAÇÃO - TTELERAD
+                        IMPLEMENTAÇÃO - TSTREAMFILE
 
 *******************************************************************************}
 
 //==| Construtor |==============================================================
-Constructor TStreamFile.Create(Owner: TComponent);
+constructor TStreamFile.Create(Owner: TComponent);
 begin
   inherited;
+
   DataStream := TMemoryStream.Create;
   FFileName  := EmptyStr;
 end;
@@ -61,14 +70,33 @@ begin
   inherited;
 end;
 
+//==| Procedimento - Atualizar Stream |=========================================
+procedure TStreamFile.RefreshStream(const AValue: Boolean);
+var
+  sAux : string;
+begin
+  sAux           := Self.FileName;
+  Self.FFileName := EmptyStr;
+  Self.FileName  := sAux;
+end;
+
 //==| Procedimento - Define nome do arquivo |===================================
 procedure TStreamFile.SetFileName(AFileName: String);
 begin
-  if FileName = AFileName then Exit;
+  if Self.FileName = AFileName then Exit;
 
   FFileName := AFileName;
-  DataStream.Clear;
-  if (FileExists(AFileName)) then DataStream.LoadFromFile(AFileName);
+
+  Self.DataStream.Clear;
+
+  if (SysUtils.FileExists(AFileName)) then
+  begin
+    Self.DataStream.LoadFromFile(AFileName);
+
+    Dialogs.ShowMessage(Self.Name
+                       + ': ' + Lib.Files.BytesToMega(Self.Data.Size)
+                       + ' carregados');
+  end;
 end;
 
 //==| Procedimento - Carrega Dados |============================================
@@ -76,9 +104,9 @@ procedure TStreamFile.LoadData(Reader: TStream);
 var
   lSize : Int64;
 begin
-  DataStream.Clear;
-  Reader.Read(lSize, SizeOf(lSize));
-  DataStream.CopyFrom(Reader, lSize);
+  Self.DataStream.Clear;
+  Reader.Read(lSize, System.SizeOf(lSize));
+  Self.DataStream.CopyFrom(Reader, lSize);
 end;
 
 //==| Procedimento - Armazena Dados |===========================================
@@ -86,10 +114,10 @@ procedure TStreamFile.StoreData(Writer: TStream);
 var
   lSize : Int64;
 begin
-  DataStream.Position := 0;
-  lSize               := DataStream.Size;
-  Writer.Write(lSize, SizeOf(lSize));
-  Writer.CopyFrom(DataStream, DataStream.Size);
+  Self.DataStream.Position := 0;
+  lSize                    := DataStream.Size;
+  Writer.Write(lSize, System.SizeOf(lSize));
+  Writer.CopyFrom(Self.DataStream, Self.DataStream.Size);
 end;
 
 //==| Procedimento - Define Propriedades |======================================
@@ -97,10 +125,11 @@ procedure TStreamFile.DefineProperties(Filer: TFiler);
 //-- Função Deve armazenar
   function ShouldStore: Boolean;
   begin
-    Result := (DataStream.Size > 0);
+    Result := (Self.DataStream.Size > 0);
   end;
 begin
   inherited;
+
   Filer.DefineBinaryProperty('Data', LoadData, StoreData, ShouldStore);
 end;
 //==============================================================================
